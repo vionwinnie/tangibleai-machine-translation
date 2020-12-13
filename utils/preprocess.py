f64257e18f9b0130f565841586d5e3644f23305d
Winnie Yeung <vionwinnie@gmail.com>
2020-12-13 09:10:45 -0800

    moved to preprocess

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -1,4 +1,28 @@
 import unicodedata,re
+import pandas as pd
+
+## Load text file as pandas dataframe
+def load_raw_text_file(file_path,num_examples=None):
+    """
+    Input: Path for raw data
+    Output: Preprocessed Dataframe
+    """
+    lines = open(file_path, encoding='UTF-8').read().strip().split('\n')
+
+    # creates lists containing each pair
+    original_word_pairs = [[w for w in l.split('\t')] for l in lines]
+
+    if num_examples:
+        original_word_pairs = original_word_pairs[:num_examples]
+
+    # Store data as a Pandas dataframe
+    df = pd.DataFrame(original_word_pairs, columns=["eng", "es", 'info'])
+
+    # Now we do the preprocessing using pandas and lambdas
+    df["eng"] = df.eng.apply(lambda w: preprocess_sentence(w))
+    df["es"] = df.es.apply(lambda w: preprocess_sentence(w))
+
+    return df
 
 
 
