4ad1355127204d38249fec595999e1786b82feed
Winnie Yeung <vionwinnie@gmail.com>
2020-12-13 11:16:17 -0800

    add device arg

--- models/modules/encoder.py
+++ models/modules/encoder.py
@@ -6,37 +6,44 @@ from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 
 
 class Encoder(nn.Module):
-    def __init__(self, vocab_size, embedding_dim, enc_units, batch_sz):
+    def __init__(self, config,vocab_size):
         super(Encoder, self).__init__()
-        self.batch_sz = batch_sz
-        self.enc_units = enc_units
+        self.gpu =  config.get('gpu',False)
+        self.batch_sz = config.get('batch_size',64)
+        self.enc_units = config.get('encoder_hidden',1024)
         self.vocab_size = vocab_size
-        self.embedding_dim = embedding_dim
+        self.embedding_dim = config.get('embedding_dim',256)
         self.embedding = nn.Embedding(self.vocab_size, self.embedding_dim)
+        self.device = torch.device("cuda" if self.gpu else "cpu")
         self.gru = nn.GRU(self.embedding_dim, self.enc_units)
-        
-    def forward(self, x, lens, device=device):
+
+    def forward(self, x, lens):
         # x: batch_size, max_length 
         
         # x: batch_size, max_length, embedding_dim
         x = self.embedding(x) 
+        print(x.shape)
                 
         # x transformed = max_len X batch_size X embedding_dim
         # x = x.permute(1,0,2)
-        x = pack_padded_sequence(x, lens) # unpad
+        x = pack_padded_sequence(x, lens)
+        print(x.shape)
     
-        self.hidden = self.initialize_hidden_state(device)
+        self.hidden = self.initialize_hidden_state(self.device)
         
         # output: max_length, batch_size, enc_units
         # self.hidden: 1, batch_size, enc_units
-        output, self.hidden = self.gru(x, self.hidden) # gru returns hidden state of all timesteps as well as hidden state at last timestep
-        
-        # pad the sequence to the max length in the batch
+        output, self.hidden = self.gru(x, self.hidden) 
+        # gru returns hidden state of all timesteps as well as hidden state at last timestep
+        print('output : {}, hidden : {}'.format(output,hidden))
+
+        # unpad the sequence to the max length in the batch
         output, _ = pad_packed_sequence(output)
+        print('output after unpack : {}'.format(output.shape))
         
         return output, self.hidden
 
-    def initialize_hidden_state(self, device=device):
-        return torch.zeros((1, self.batch_sz, self.enc_units)).to(device)
+    def initialize_hidden_state(self):
+        return torch.zeros((1, self.batch_sz, self.enc_units)).to(self.device)
 
 
