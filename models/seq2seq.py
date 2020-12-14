78d773e6fe978ef680f5b95de1831228d6ded804
Winnie Yeung <vionwinnie@gmail.com>
2020-12-13 21:39:29 -0800

    bug

--- models/seq2seq.py
+++ models/seq2seq.py
@@ -52,7 +52,7 @@ class Seq2Seq(nn.Module):
         ## Check to see if batch_size parameter is fixed or base on input batch
         cur_batch_size = x.size()[1]
         encode_init_state = self.encoder.initialize_hidden_state(cur_batch_size)
-        encoder_outputs, encoder_state = self.encoder.forward(x, init_state, x_len)
+        encoder_outputs, encoder_state = self.encoder.forward(x, encode_init_state, x_len)
 
         return encoder_outputs, encoder_state
 
